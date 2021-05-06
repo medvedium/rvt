174f49b95351837fdd5e67f59e80c34f94d2880c
medvedium <mumbles2606@gmail.com>
2021-05-06 14:11:53 +0300

    added js

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,87 @@
-function testWebP(callback) {
-  var webP = new Image();
-  webP.onload = webP.onerror = function () {
-    callback(webP.height == 2);
-  };
-  webP.src =
-    "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-}
+let flatType = document.querySelector('#flatType'),
+  square = document.querySelector('#square'),
+  squareRange = document.querySelector('#squareRange'),
+  electricity = document.querySelector('#electricity'),
+  fixType = document.querySelector('#fixType'),
+  roof = document.querySelector('#roof'),
+  toilet = document.querySelector('#toilet'),
+  toiletSquare = document.querySelector('#toiletSquare'),
+  toiletSquareLabel = document.querySelector('#toiletSquareLabel'),
+  toiletSquareRange = document.querySelector('#toiletSquareRange'),
+  floor = document.querySelector('#floor'),
+  doors = document.querySelector('#doors'),
+  total = document.querySelector('#total');
 
-testWebP(function (support) {
-  if (support == true) {
-    document.querySelector("body").classList.add("webp");
-  } else {
-    document.querySelector("body").classList.add("no-webp");
-  }
+let result;
+let render = function () {
+  let a = flatType.value,
+    s = squareRange.value,
+    b = electricity.value,
+    c = fixType.value,
+    d = roof.value,
+    e = floor.value,
+    f = toilet.value,
+    ts = toiletSquareRange.value,
+    g = doors.value;
+
+  square.value = `${squareRange.value} кв. м`;
+  toiletSquare.value = `${toiletSquareRange.value} кв. м`;
+
+  result = a * s + b * s + c * s + d * s + e * s + f * ts + g * 8500;
+
+  console.log(result);
+  total.innerHTML = result;
+};
+render();
+
+toiletSquare.classList.add('hide');
+toiletSquareLabel.classList.add('hide');
+toiletSquareRange.classList.add('hide');
+
+flatType.addEventListener('change', () => {
+  render();
 });
+squareRange.addEventListener('change', () => {
+  render();
+});
+electricity.addEventListener('change', () => {
+  render();
+});
+fixType.addEventListener('change', () => {
+  render();
+});
+roof.addEventListener('change', () => {
+  render();
+});
+toilet.addEventListener('change', () => {
+  if (toilet.value === '13000') {
+    toiletSquare.classList.remove('hide');
+    toiletSquareLabel.classList.remove('hide');
+    toiletSquareRange.classList.remove('hide');
+
+    toiletSquare.classList.add('show');
+    toiletSquareLabel.classList.add('show');
+    toiletSquareRange.classList.add('show');
+  } else {
+    toiletSquare.classList.remove('show');
+    toiletSquareLabel.classList.remove('show');
+    toiletSquareRange.classList.remove('show');
+
+    toiletSquare.classList.add('hide');
+    toiletSquareLabel.classList.add('hide');
+    toiletSquareRange.classList.add('hide');
+
+  }
+  render();
+});
+toiletSquareRange.addEventListener('change', () => {
+  render();
+});
+floor.addEventListener('change', () => {
+  render();
+});
+doors.addEventListener('change', () => {
+  render();
+});
+
+
